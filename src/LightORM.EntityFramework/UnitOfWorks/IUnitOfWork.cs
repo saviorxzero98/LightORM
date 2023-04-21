6d25c725d087fa58e5099407c5b3f7d196409a59
Marty Chen <marty_chen@mail.gss.com.tw>
2023-04-21 16:58:12 +0800

    add: entity framework with pattern

---
+++ src/LightORM.EntityFramework/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LightORM.EntityFramework.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        // <summary>
+        /// Database Context
+        /// </summary>
+        public DbContext Context { get; }
+
+        /// <summary>
+        /// Saves the change.
+        /// </summary>
+        /// <returns></returns>
+        void SaveChange();
+    }
+}
---
+++ src/LightORM.EntityFramework/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LightORM.EntityFramework.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; private set; }
+
+        public UnitOfWork(DbContext context)
+        {
+            Context = context;
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+
+        public void SaveChange()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
